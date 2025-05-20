b1a7eecdbe6e9c2815dd6be494580f00388cc264
someloki <tenacity686@gmail.com>
2025-05-20 00:04:02 -0700

    Feat: Add new book modal; finish display library

--- script.js
+++ script.js
@@ -9,22 +9,36 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.id = crypto.randomUUID();
-/*   this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  }; */
+}
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call this constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
 }
 
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ` + (this.read ? "have read" : "have not read yet");
 };
 
-const createElement = function( element, ElementID, textContent, appendTo) {
+const createElement = function( element, elementClass, textContent, appendTo, elementId) {
+  const elem = document.createElement(element);
+  // this is to always select the last book-card
+  const parent = document.querySelector(`${appendTo}:last-of-type`);
   if (!textContent) {
     textContent = "";
   }
-  const elem = document.createElement(element);
-  const parent = document.querySelector(`${appendTo}`);
-  elem.id = `${ElementID}`;
+  if (elementClass) {
+    elem.classList.add(elementClass);
+  }
+  if (elementId) {
+    elem.id = elementId;
+  }
   elem.textContent = textContent;
   parent.appendChild(elem);
 }
@@ -33,23 +47,39 @@ function addBookToLibrary( title, author, pages, read) {
   const newBook = new Book( title, author, pages, read);
   console.log(newBook);
   myLibrary.push(newBook);
-  return;
 };
 
-
-
 function displayLibrary() {
-  for ( book of myLibrary) {
-    createElement("div"
-
+  // rare instance of using for (...in...) need the index not the full book object
+  for ( const book in myLibrary) {
+    createBookCard();
+    createElement( "div", "", myLibrary[book].info(), ".book-card", myLibrary[book].id);
   }
-
 }
 
+function createBookCard() {
+  createElement( "div", "book-card", "", ".bookshelf")
+}
 
 const theHobbit = addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
-
+const the = addBookToLibrary("The ", "J.R.R Tolkien", 295, false);
+const hobbit = addBookToLibrary("Hobbit", "J.R.R Tolkien", 295, false);
+const obbit = addBookToLibrary("The test", "J.R.R Tolkien", 295, false);
 
 console.log(theHobbit);
 console.log(myLibrary);
 console.log(myLibrary[0].info());
+
+const newBookDialog = document.querySelector("dialog");
+const newButton = document.querySelector(".new-book");
+const cancelButton = document.querySelector(".cancel");
+
+// "Show the dialog" button opens the dialog modally
+newButton.addEventListener("click", () => {
+  newBookDialog.showModal();
+});
+
+// "Close" button closes the dialog
+cancelButton.addEventListener("click", () => {
+  newBookDialog.close();
+});
