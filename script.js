0583c957d874d51efcd9a966f7d57b670bf0fefc
someloki <tenacity686@gmail.com>
2025-05-21 22:11:14 -0700

    Feat: Finish book modal and remove func

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -11,7 +11,8 @@ function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
-function Book(title, author, pages, read) {
+// use this later for creating buttons
+/* function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call this constructor");
   }
@@ -20,13 +21,13 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.id = crypto.randomUUID();
-}
+} */
 
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ` + (this.read ? "have read" : "have not read yet");
 };
 
-const createElement = function( element, elementClass, textContent, appendTo, elementId) {
+const createElement = function( element, elementClass, textContent, appendTo, associateBook) {
   const elem = document.createElement(element);
   // this is to always select the last book-card
   const parent = document.querySelector(`${appendTo}:last-of-type`);
@@ -36,8 +37,8 @@ const createElement = function( element, elementClass, textContent, appendTo, el
   if (elementClass) {
     elem.classList.add(elementClass);
   }
-  if (elementId) {
-    elem.id = elementId;
+  if (associateBook) {
+    elem.setAttribute("data-related-book", associateBook);
   }
   elem.textContent = textContent;
   parent.appendChild(elem);
@@ -45,37 +46,41 @@ const createElement = function( element, elementClass, textContent, appendTo, el
 
 function addBookToLibrary( title, author, pages, read) {
   const newBook = new Book( title, author, pages, read);
-  console.log(newBook);
   myLibrary.push(newBook);
 };
 
 function displayLibrary() {
-  // rare instance of using for (...in...) need the index not the full book object
   for ( const book in myLibrary) {
-    createBookCard();
+  
+    createBookCard(myLibrary[book].id);
     createElement( "div", "", myLibrary[book].info(), ".book-card", myLibrary[book].id);
   }
 }
 
-function createBookCard() {
-  createElement( "div", "book-card", "", ".bookshelf")
+function removeBook(bookToRemove) {
+  const removeList = document.querySelectorAll(`[data-related-book="${bookToRemove}"]`);
+  for (let element of removeList) {
+    element.remove();
+}
 }
 
+function createBookCard(associateBook) {
+  createElement( "div", "book-card", "", ".bookshelf", associateBook)
+}
+
+// these are tests to remove later
 const theHobbit = addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
 const the = addBookToLibrary("The ", "J.R.R Tolkien", 295, false);
 const hobbit = addBookToLibrary("Hobbit", "J.R.R Tolkien", 295, false);
 const obbit = addBookToLibrary("The test", "J.R.R Tolkien", 295, false);
 
-console.log(theHobbit);
-console.log(myLibrary);
-console.log(myLibrary[0].info());
-
 const newBookDialog = document.querySelector("dialog");
-const newButton = document.querySelector(".new-book");
+const startNewButton = document.querySelector(".new-book");
 const cancelButton = document.querySelector(".cancel");
+const addNewButton = document.querySelector(".add-book");
 
 // "Show the dialog" button opens the dialog modally
-newButton.addEventListener("click", () => {
+startNewButton.addEventListener("click", () => {
   newBookDialog.showModal();
 });
 
@@ -83,3 +88,18 @@ newButton.addEventListener("click", () => {
 cancelButton.addEventListener("click", () => {
   newBookDialog.close();
 });
+
+addNewButton.addEventListener("click", (e) => {
+ const title = document.querySelector(`#book-title`).value
+ const author = document.querySelector(`#book-author`).value
+ const pages = document.querySelector(`#book-pages`).value
+ const read = document.querySelector(`#book-read`).value
+ if (!title || !author || !pages) {
+  return;
+ }
+  addBookToLibrary( title, author, pages, read);
+  newBookDialog.close();
+  e.preventDefault();
+});
+
+displayLibrary()
